e30dd3b7b4b492d39c5a59b8c8a11d2ac72a7a1f
AnnaFlourou <flourou.anna@gmail.com>
2018-05-02 15:41:23 +0200

    finish assign1

--- src/app/assignment2/assignment2.module.ts
+++ src/app/assignment2/assignment2.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Assignment2Component } from './assignment2.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
   ],
+  declarations: [Assignment2Component],
 })
 export class Assignment2Module { }
